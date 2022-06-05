aa949f9d1418bb91a6a91a0db2a707f758c33612
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-05 19:31:59 +0900

    코드 정리용 클래스 - BaseActivity 추가  - 모든 화면의 공통 기능 일괄 구현에 사용 예정

---
+++ app/src/main/java/com/start/a20220605_colosseum/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.start.a20220605_colosseum
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity() {
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
