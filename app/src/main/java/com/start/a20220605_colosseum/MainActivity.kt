e69d30f389f2df9acb3af496c96f80c8ec613a49
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-05 19:11:59 +0900

    binding 변수 생성 / 초기화

--- app/src/main/java/com/start/a20220605_colosseum/MainActivity.kt
+++ app/src/main/java/com/start/a20220605_colosseum/MainActivity.kt
@@ -2,12 +2,15 @@ package com.start.a20220605_colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.start.a20220605_colosseum.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
     }
 }
