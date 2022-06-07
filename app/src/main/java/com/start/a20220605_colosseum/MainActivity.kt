36df45e74af9a8c25823414594badab990af16c5
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-07 22:42:49 +0900

    토론 주제 서버에서 받아와서 파싱 / ArrayList에 추가  - 목록(JSONArray) 파싱  - for문으로 반복파싱/ ArrayList에 추가

--- app/src/main/java/com/start/a20220605_colosseum/MainActivity.kt
+++ app/src/main/java/com/start/a20220605_colosseum/MainActivity.kt
@@ -3,12 +3,15 @@ package com.start.a20220605_colosseum
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.start.a20220605_colosseum.databinding.ActivityMainBinding
+import com.start.a20220605_colosseum.datas.TopicData
 import com.start.a20220605_colosseum.utils.ServerUtil
 import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
+
+    val mTopicList = ArrayList<TopicData>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,8 +28,37 @@ class MainActivity : BaseActivity() {
 //        getMyInfoFromServer()
 
 //        실제 - 메인 화면의 데이터 받아오기 -> 토론 주제 목록 ->리스트뷰로 표기
-
+        getTopicListFromSever()
     }
+
+    fun getTopicListFromSever(){
+
+        ServerUtil.getRequestMainInfo(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val topicsArr = dataObj.getJSONArray("topics")
+
+                for(i in 0 until topicsArr.length()){
+//                    하나의 토론 주제를 표한하는 { } 추출.
+
+                    val topicObj = topicsArr.getJSONObject(i)
+
+//                    목록에 뿌려줄 TopicData 형태로 변환
+                    val topicData = TopicData()
+                    topicData.id = topicObj.getInt("id")
+                    topicData.title = topicObj.getString("title")
+                    topicData.imageURL = topicObj.getString("img_url")
+
+//                    완성된 topicData => mTopicList에 추가.
+                    mTopicList.add(topicData)
+                }
+
+            }
+
+        })
+    }
+
 
 //    fun getMyInfoFromServer(){
 //
