9a49dec9638e4f7ad1b1e58edb9b9dc61a3cb78a
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-09 14:24:17 +0900

    댓글 - 선택 진영 파싱 / 표시

--- app/src/main/java/com/start/a20220605_colosseum/datas/ReplyData.kt
+++ app/src/main/java/com/start/a20220605_colosseum/datas/ReplyData.kt
@@ -8,6 +8,9 @@ class ReplyData {
 
     var writer = UserData() //사용자 데이터가 들어올거라고 명시.
 
+//    어느 진영에 대한 댓글?
+    var selectedSide = SideData()
+
     companion object {
 
         fun getReplyDataFromJson(jsonObj : JSONObject) : ReplyData {
@@ -19,6 +22,9 @@ class ReplyData {
 
             val userObj = jsonObj.getJSONObject("user")
             replyData.writer = UserData.getUserDataFromJson(userObj)
+
+            val sideObj = jsonObj.getJSONObject("selected_side")
+            replyData.selectedSide = SideData.getSideDataFromJson(sideObj)
 
             return replyData
 
