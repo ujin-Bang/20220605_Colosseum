1c6d9d0248f3137bccc3a6a4ca2a1c6b55efa4ec
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-09 11:54:43 +0900

    댓글 어댑터 클래스 추가  - 복붙 / 일부 수정

---
+++ app/src/main/java/com/start/a20220605_colosseum/adapters/ReplyAdapter.kt
@@ -0,0 +1,37 @@
+package com.start.a20220605_colosseum.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.start.a20220605_colosseum.R
+import com.start.a20220605_colosseum.datas.ReplyData
+import com.start.a20220605_colosseum.datas.TopicData
+
+class ReplyAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: List<ReplyData>) : ArrayAdapter<ReplyData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if( tempRow == null){
+
+            tempRow = mInflater.inflate(R.layout.reply_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+        return row
+    }
+}
