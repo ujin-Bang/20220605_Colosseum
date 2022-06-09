4ff81fb97430e0c0bd6339dadf02660b70fc9411
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-09 15:15:23 +0900

    시작 화면(SplashActivity)임시 추가

---
+++ app/src/main/java/com/start/a20220605_colosseum/SplashActivity.kt
@@ -0,0 +1,26 @@
+package com.start.a20220605_colosseum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.start.a20220605_colosseum.databinding.ActivitySplashBinding
+
+class SplashActivity : BaseActivity() {
+
+    lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_splash)
+        setupEvents()
+        setValues()
+    }
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
