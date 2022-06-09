97607a24cb73fdc65d1e959520e23dd0710f6ab6
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-09 16:04:54 +0900

    댓글 작성 일시 파싱  - 서버 : String -> SimpleDateFormat -> 데이터 변수: Calendar로 저장

--- app/src/main/java/com/start/a20220605_colosseum/datas/ReplyData.kt
+++ app/src/main/java/com/start/a20220605_colosseum/datas/ReplyData.kt
@@ -1,6 +1,8 @@
 package com.start.a20220605_colosseum.datas
 
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
 
 class ReplyData {
     var id = 0
@@ -10,6 +12,9 @@ class ReplyData {
 
 //    어느 진영에 대한 댓글?
     var selectedSide = SideData()
+
+//    언제 적힌 댓글?
+    var createdAt = Calendar.getInstance() //기본 Calendar 대입 -> 현재일시가 저장됨.
 
     companion object {
 
@@ -26,6 +31,16 @@ class ReplyData {
             val sideObj = jsonObj.getJSONObject("selected_side")
             replyData.selectedSide = SideData.getSideDataFromJson(sideObj)
 
+//            작성일시 파싱 -> String으로 서버가 내려줌.(String으로 임시 추출)
+            val createdAtStr = jsonObj.getString("created_at")
+
+//            임시로 추출한 (String -> Calendar)형태로 변환 -> 댓글데이터의 작성일시로 반영.
+
+//            서버가 준 String을 분석하기 위한 SimpleDateFormat 만들기
+            val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+
+//            parse로 Date형태의 일시 생성 -> Calendar변수.time에 대입.
+            replyData.createdAt.time = sdf.parse(createdAtStr)
             return replyData
 
         }
