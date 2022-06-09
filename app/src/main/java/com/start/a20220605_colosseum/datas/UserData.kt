2b591a9040f4cb2bbc7a4db5b02ae31bd18a88b1
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-09 12:54:00 +0900

    댓글 추가 파싱 중 - 사용자 데이터 발견  - 데이터 클래스 추가 + 파싱 함수 추가

---
+++ app/src/main/java/com/start/a20220605_colosseum/datas/UserData.kt
@@ -0,0 +1,26 @@
+package com.start.a20220605_colosseum.datas
+
+import org.json.JSONObject
+
+class UserData {
+
+    var id = 0
+    var email = ""
+    var nickname = ""
+
+    companion object {
+
+        fun getUserDataFromServer(jsonObj : JSONObject) : UserData {
+
+            val userData = UserData()
+
+            userData.id = jsonObj.getInt("id")
+            userData.email = jsonObj.getString("email")
+            userData.nickname = jsonObj.getString("nick_name")
+
+            return userData
+
+        }
+
+    }
+}
