eddca33d1b750408ad2642115e1f84c96aa677e5
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-10 15:02:02 +0900

    의견 남기기 API호출

--- app/src/main/java/com/start/a20220605_colosseum/WriteReplyActivity.kt
+++ app/src/main/java/com/start/a20220605_colosseum/WriteReplyActivity.kt
@@ -2,9 +2,12 @@ package com.start.a20220605_colosseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.start.a20220605_colosseum.databinding.ActivityWriteReplyBinding
 import com.start.a20220605_colosseum.datas.TopicData
+import com.start.a20220605_colosseum.utils.ServerUtil
+import org.json.JSONObject
 
 class WriteReplyActivity : BaseActivity() {
 
@@ -20,6 +23,41 @@ class WriteReplyActivity : BaseActivity() {
 
     }
     override fun setupEvents() {
+        
+        binding.btnOk.setOnClickListener { 
+            
+//            입력한 문구가 몇글자? -> 10글자 이내면 거부
+             val inputContent = binding.edtContent.text.toString()
+            
+            if (inputContent.length < 10){
+
+                Toast.makeText(mContext, "최소 10글자 이상 입력해주세요.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+//            검사 통과 -> 의견등록 (서버 호출)
+
+            ServerUtil.postRequestTopicReply(mContext, mTopicData.id, inputContent, object : ServerUtil.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    runOnUiThread {
+                        if(code == 200){
+                            Toast.makeText(mContext, "의견등록 성공", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+                        else {
+                            val message = jsonObj.getString("message")
+                            Toast.makeText(mContext, message, Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+            })
+
+        }
 
 
     }
