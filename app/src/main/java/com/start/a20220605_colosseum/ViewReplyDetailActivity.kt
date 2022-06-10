731ac6c68eec1eadf96cc2d4fabc59d572b6bb11
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-10 16:40:19 +0900

    넘겨받은 데이터 활용 - UI에 반영  - 클래스의 여러 하위 클래스들도 활용

--- app/src/main/java/com/start/a20220605_colosseum/ViewReplyDetailActivity.kt
+++ app/src/main/java/com/start/a20220605_colosseum/ViewReplyDetailActivity.kt
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.start.a20220605_colosseum.databinding.ActivityViewReplyDetailBinding
+import com.start.a20220605_colosseum.datas.ReplyData
 
 class ViewReplyDetailActivity : BaseActivity() {
 
     lateinit var bindiing: ActivityViewReplyDetailBinding
+
+    lateinit var mReplyData : ReplyData
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindiing = DataBindingUtil.setContentView(this,R.layout.activity_view_reply_detail)
@@ -20,6 +23,12 @@ class ViewReplyDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mReplyData = intent.getSerializableExtra("reply") as ReplyData
+
+        bindiing.txtSelectedSide.text = "(${mReplyData.selectedSide.title})"
+        bindiing.txtWriterNickname.text = mReplyData.writer.nickname
+        bindiing.txtContent.text = mReplyData.content
 
     }
 
