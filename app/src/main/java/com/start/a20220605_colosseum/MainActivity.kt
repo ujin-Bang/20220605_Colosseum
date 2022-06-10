90ef4d793b63a9084ac7d82aed9d98cbfa42187c
ujin-Bang <91653684+ujin-Bang@users.noreply.github.com>
2022-06-10 10:54:29 +0900

    메인화면에서는 따로 뒤로가기 버튼 숨김 처리

--- app/src/main/java/com/start/a20220605_colosseum/MainActivity.kt
+++ app/src/main/java/com/start/a20220605_colosseum/MainActivity.kt
@@ -2,6 +2,7 @@ package com.start.a20220605_colosseum
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.databinding.DataBindingUtil
 import com.start.a20220605_colosseum.adapters.TopicAdapter
 import com.start.a20220605_colosseum.databinding.ActivityMainBinding
@@ -49,6 +50,10 @@ class MainActivity : BaseActivity() {
         mTopicAdapter = TopicAdapter(mContext, R.layout.topic_list_item, mTopicList)
 
         binding.topicListView.adapter = mTopicAdapter
+
+//        액션바의 뒤로가기 버튼 숨김(상속받은 변수 활용)
+
+        btnBack.visibility = View.GONE
     }
 
     fun getTopicListFromSever(){
